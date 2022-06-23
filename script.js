010ec294efb69e646c347506c19c400f3abed75f
fedepaloz <palozzifederico@gmail.com>
2022-06-23 17:26:13 +0200

    calcolo multipli

--- script.js
+++ script.js
@@ -5,3 +5,27 @@ Prima di partire a scrivere codice poniamoci qualche domanda:
 Come faccio a sapere se un numero è divisibile per un altro?
 Abbiamo visto qualcosa di particolare che possiamo usare? */
 
+//se x è divisibile per 3 allora il risultato è fizz
+//se x è divisibile per 5 allora il risultato è buzz 
+//se x è divisibile per 3 e per 5 allora il risultato è fizzbuzz
+
+
+
+
+
+
+
+
+
+
+for (let i = 1 ; i <= 100 ; i++) {
+  
+  if (i % 15 == 0 ) {console.log("fizzbuzz")}
+  else if (i % 3 == 0) {console.log("fizz")}
+  else if (i % 5 == 0) {console.log("buzz")}
+ 
+
+  console.log(i)
+
+}
+
